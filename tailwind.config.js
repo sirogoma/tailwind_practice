ef2a52216e6762a75bed2d4a561f4a17b8e2e9aa
sirotan <shiro.0630.ms@gmail.com>
2022-11-05 18:38:14 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/pages/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans JP"']
+      }
+    }
+  },
+  plugins: []
+}
